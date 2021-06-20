0d1217cce4962019f08065cc1ba8fef3c20601f5
Jeffrey Wang <j@mywikis.com>
2021-06-20 01:06:54 -0500

    MVP of NamespacesManager extension

---
+++ includes/NamespaceManagerHooks.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * NamespaceManager
+ * Hooks
+ * @author Jeffrey Wang for MyWikis LLC
+ */
+
+public class NamespaceManagerHooks {
+    public function onCanonicalNamespaces(array &$namespaces) {
+        $data = loadNamespaceData();
+
+        $count = 3000;
+        foreach ($data as $namespaceDefinition) {
+            $namespaceName = $namespaceDefinition['name'];
+            if ($namespaceName === null) {
+                wfDebugLog('NamespaceManager', 'Invalid namespace definition.');
+                return;
+            }
+            str_replace(' ', '_', $namespaceName);
+            $namespaces[$count++] = $namespaceName;
+            $namespaces[$count++] = $namespaceName . '_talk';
+        }
+    }
+
+    /**
+     * Retrieves JSON files
+     * @return associated_array if success
+     *         false if failure
+    */
+    private function loadNamespaceData() {
+        global $wgNamespaceManagerDir;
+        
+        $fileContents = file_get_contents(__DIR__ . '/../' . $wgNamespaceManagerDir);
+        if ($fileContents === false) {
+            return false;
+        }
+        $data = json_decode($fileContents, true);
+        return $data ?? false;
+    }
+}
