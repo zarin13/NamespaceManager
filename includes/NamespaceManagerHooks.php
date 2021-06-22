5df5f02cbaaec462b414d6e08884519165f4dce7
Jeffrey Wang <j@mywikis.com>
2021-06-22 01:22:14 -0500

    Removing data type specification in onMediaWikiServices hook overall

--- includes/NamespaceManagerHooks.php
+++ includes/NamespaceManagerHooks.php
@@ -12,7 +12,7 @@ class NamespaceManagerHooks {
      * initialized, believe it or not.
      * i.e. This is called before the CanonicalNamespaces hook is called.
      */
-    public static function onMediaWikiServices(MediaWikiServices &$services) {
+    public static function onMediaWikiServices(&$services) {
         global $wgCapitalLinkOverrides,
             $wgCapitalLinks,
             $wgContentNamespaces,
